03f7fcfb5a1e6e9bca44a227daae040d18b21ee8
Ami <211770756@qq.com>
2021-09-04 10:44:34 +0800

    config和bus

---
+++ cloud-config-client3366/src/main/java/com/sunday/ConfigClient3366.java
@@ -0,0 +1,18 @@
+package com.sunday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Description: TODO(这里用一句话描述这个类的作用)
+ * @Author Ami
+ * @Date 2021/9/2 11:12 下午
+ */
+@SpringBootApplication
+@EnableEurekaClient
+ public class ConfigClient3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClient3366.class, args);
+    }
+}
